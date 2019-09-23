e45046efa7016e49b2b4c8b835b96ba09283fd5d
yuan_yl <827543954@qq.com>
2019-09-23 15:33:27 +0800

    注册中心替换为eureka

--- kitty-admin/src/main/java/com/louis/kitty/admin/KittyAdminApplication.java
+++ kitty-admin/src/main/java/com/louis/kitty/admin/KittyAdminApplication.java
@@ -2,14 +2,13 @@ package com.louis.kitty.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 /**
  * 启动器
  * @author Louis
  * @date Oct 29, 2018
  */
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication(scanBasePackages={"com.louis.kitty"})
 public class KittyAdminApplication {
 
